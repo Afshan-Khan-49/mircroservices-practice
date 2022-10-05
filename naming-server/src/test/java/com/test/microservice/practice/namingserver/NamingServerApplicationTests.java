ce3102d6aaedd47a8c960b0b1dc50a8e69368d3f
Afshan-Khan-49 <afshan97khan@gmail.com>
2022-10-05 19:49:00 +0530

    Adding eureka naming server

---
+++ naming-server/src/test/java/com/test/microservice/practice/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.microservice.practice.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
